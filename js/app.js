b7807ccd3ae1f56a31216992c5d301298e549049
Matt Rhone <kritoke@gmail.com>
2017-10-25 18:22:29 -0500

    Add some initial variables.

--- js/app.js
+++ js/app.js
@@ -48,7 +48,8 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var player = {},
+    enemy = [];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
