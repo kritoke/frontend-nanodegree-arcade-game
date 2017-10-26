81316079adea3a39161e05b5bb168b27056d1ac4
Matt Rhone <kritoke@gmail.com>
2017-10-26 17:55:29 -0500

    Added ability for player to get drawn on board.

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ Player.prototype.update = function() {
 };
 
 Player.prototype.render = function() {
-
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 Player.prototype.handleInput = function(keyPressed) {
